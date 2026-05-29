9cd31a2d623f2b55a6d000063ab29f6727afc49d
AnisJshK <anisshaikh2131@gmail.com>
2026-05-29 18:21:55 +0530

    added-auth

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,30 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import Navbar from './pages/Navbar'
+import Navbar from './components/Navbar'
 import Chatpanel from './pages/Chatpanel' 
 import ContactsPanel from './pages/ContactsPanel'
 import SettingsPanel from './pages/SettingsPanel'
 import ChatLayout from './pages/ChatLayout'
 import EmptyChatState from './pages/EmptyChatState'
+import SignInPage from './components/SignInPage'
+import SignUpPage from './components/SignUpPage'
+import HomePage from './pages/homePage'
+import Layout from './components/Layout'
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div className="flex h-screen w-screen flex-col md:flex-row bg-slate-950 text-slate-100 overflow-hidden">
-        
-        <Navbar />
+       
+        <Routes>
+          <Route path='/' element={<HomePage/>}/>
+
+       
         
         {/* Main page viewport content area */}
-        <div className="flex-1 h-full overflow-hidden pb-16 md:pb-0">
-          <Routes>
-            <Route path="/" element={<Navigate to="/chats" replace />} />
+          <Route element={<Layout/>}>
+            
+            <Route path='/sign-in/*' element={<SignInPage/>} />
+            <Route path='/sign-up/*' element={<SignUpPage/>} />
             
             <Route path='/chats' element={<ChatLayout />}>
               <Route index element={<EmptyChatState />} />
@@ -26,11 +33,10 @@ const App = () => {
             
             <Route path='/contacts' element={<ContactsPanel />} />
             <Route path='/settings' element={<SettingsPanel />} />
-            <Route path="*" element={<Navigate to="/chat" replace />} />
-          </Routes>
-        </div>
+          </Route>
 
-      </div>
+        </Routes>
+
     </BrowserRouter>
   )
 }
---
+++ client/src/components/Layout.jsx
@@ -0,0 +1,22 @@
+import { Outlet, Route, useLocation } from "react-router-dom";
+import Navbar from "./Navbar";
+
+const Layout = () => {
+  const location = useLocation();
+
+  const hideNavbarRoutes = ["/sign-in","/sign-up"];
+
+  const hideNavbar = location.pathname==='/'|| hideNavbarRoutes.some((route)=>location.pathname.startsWith(route))
+
+  return (
+    <div className="flex h-screen w-screen flex-col md:flex-row bg-slate-950 text-slate-100 overflow-hidden">
+      {!hideNavbar && <Navbar />}
+
+      <div className="flex-1 h-full overflow-hidden pb-16 md:pb-0">
+        <Outlet />
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
